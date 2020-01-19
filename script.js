0ed8279a98cceb464093266d148d37b642b87d93
Renren0015 <57824147+Renren0015@users.noreply.github.com>
2020-01-20 02:47:26 +0800

    Activity

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $("#table").click(function() {
+        $("#container").load("table.html");
+    });
+});
+$(document).ready(function() {
+    $("#dialog").click(function() {
+        $("#container").load("dialog.html");
+    });
+});
